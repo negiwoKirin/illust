31b5e77a2c1f4e85711b6544302fb90456389be0
negiwokirin <hr.u.acc@gmail.com>
2020-03-18 22:55:08 +0900

    vuetify 暗中模索中

---
+++ front/src/components/bar/NavBar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <v-app-bar
+      color="indigo"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn> -->
+
+
+
+
+
+    <v-btn icon>
+        <search id="clickaway" v-on-clickaway="searchFalse" v-show="search_box" @click="searchFalse()"></search>
+        <v-icon @click="clickSearch()">mdi-magnify</v-icon>
+    </v-btn>
+
+
+      <!-- <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn> -->
+        <!-- </template> -->
+
+        <!-- <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list> -->
+      <!-- </v-menu> -->
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import Search from './search/Search.vue';
+import { mixin as clickaway } from 'vue-clickaway';
+
+export default {
+    components: {Search},
+    mixins: [ clickaway ],
+    data() {
+        return {
+            search_box: false
+        }
+    },
+    methods: {
+        clickSearch() {
+            this.search_box = !this.search_box
+        },
+        searchFalse() {
+            this.search_box = false
+        }
+    }
+}
+</script>
---
+++ front/src/components/bar/search/Search.vue
@@ -0,0 +1,75 @@
+
+<template>
+  <v-card
+    color="indigo"
+    dark
+  >
+    <v-card-title class="headline red lighten-3">
+      Search for Public APIs
+    </v-card-title>
+    <v-card-text>
+      Explore hundreds of free API's ready for consumption! For more information visit
+      <a
+        class="grey--text text--lighten-3"
+        href="https://github.com/toddmotto/public-apis"
+        target="_blank"
+      >the Github repository</a>.
+    </v-card-text>
+    <v-card-text>
+      <v-autocomplete
+        v-model="model"
+        :items="items"
+        :loading="isLoading"
+        :search-input.sync="search"
+        color="white"
+        hide-no-data
+        hide-selected
+        item-text="Description"
+        item-value="API"
+        label="Public APIs"
+        placeholder="Start typing to Search"
+        prepend-icon="mdi-database-search"
+        return-object
+      ></v-autocomplete>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-expand-transition>
+      <v-list v-if="model" class="red lighten-3">
+        <v-list-item
+          v-for="(field, i) in fields"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="field.value"></v-list-item-title>
+            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-expand-transition>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn
+        :disabled="!model"
+        color="grey darken-3"
+        @click="model = null"
+      >
+        Clear
+        <v-icon right>mdi-close-circle</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            model: null,
+            items: null,
+            isLoading: false,
+            search: null,
+            fields: [],
+        }
+    }
+}
+</script>
