4f0cec1479f8fc7d0c23a68648ef421574542027
negiwokirin <hr.u.acc@gmail.com>
2020-03-22 21:55:31 +0900

    usersテーブルからnameカラム削除

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('login_id');
             $table->string('password');
             $table->rememberToken();
