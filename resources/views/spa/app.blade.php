a29d438bf15f5f72ac77d66a6e13115b491fdfd6
negiwokirin <hr.u.acc@gmail.com>
2020-03-22 21:58:09 +0900

    bladeの整理

--- resources/views/spa/app.blade.php
+++ resources/views/spa/app.blade.php
@@ -1,1 +1,20 @@
-<!DOCTYPE html><html lang=en><head><meta charset=utf-8><meta http-equiv=X-UA-Compatible content="IE=edge"><meta name=viewport content="width=device-width,initial-scale=1"><link rel=icon href=/app/favicon.ico><title>front</title><link href=/app/js/about.7c2f9500.js rel=prefetch><link href=/app/css/chunk-vendors.2408c6c3.css rel=preload as=style><link href=/app/js/app.02980d3d.js rel=preload as=script><link href=/app/js/chunk-vendors.8a029334.js rel=preload as=script><link href=/app/css/chunk-vendors.2408c6c3.css rel=stylesheet></head><body><noscript><strong>We're sorry but front doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id=app></div><script src=/app/js/chunk-vendors.8a029334.js></script><script src=/app/js/app.02980d3d.js></script></body></html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="icon" href="/app/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+
+    <title>front</title>
+  <link href="/app/js/about.js" rel="prefetch"><link href="/app/js/app.js" rel="preload" as="script"><link href="/app/js/chunk-vendors.js" rel="preload" as="script"></head>
+  <body>
+    <noscript>
+      <strong>We're sorry but front doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+    </noscript>
+    <div id="app"></div>
+    <!-- built files will be auto injected -->
+  <script type="text/javascript" src="/app/js/chunk-vendors.js"></script><script type="text/javascript" src="/app/js/app.js"></script></body>
+</html>
