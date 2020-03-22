e7a5e9d1970cacb53597f5d80f8069c08717b626
negiwokirin <hr.u.acc@gmail.com>
2020-03-22 21:54:54 +0900

    ルーティング設定

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
+});
+
+
+Route::group(['middleware' => ['api']], function () {
+    Route::resource('login', 'Api\LoginController');
 });
